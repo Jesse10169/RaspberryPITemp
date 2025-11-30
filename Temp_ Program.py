b20ae39d9f637b8b6f6b0347961080368c100e38
Jesse Fuentes <jessefuentes@Jesses-MacBook-Pro.local>
2025-11-29 18:42:24 -0600

    Updated Temp_ Program.py loop logic

--- Temp_ Program.py
+++ Temp_ Program.py
@@ -6,7 +6,7 @@ import csv
 DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 4 
 
-while True:
+for i in range(10):
     humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
 
     if humidity is not None and temperature is not None:
