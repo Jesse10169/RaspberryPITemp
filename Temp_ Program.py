6d18e3c79db7a6099ede222edce9f8a283956622
Jesse Fuentes <jessefuentes@Jesses-MacBook-Pro.local>
2025-11-29 19:18:33 -0600

    Updated Temp_Program.py changed on were the data is stored

--- Temp_ Program.py
+++ Temp_ Program.py
@@ -14,7 +14,7 @@ for i in range(10):
         print("Temperature={0:0.1f}C  Humidity={1:0.1f}%".format(
             temperature_Fahrenheit, humidity))
     
-        with open("log.csv", "a", newline='') as file:
+        with open("Stored_Data.csv", "a", newline='') as file:
             writer = csv.writer(file)
             writer.writerow([temperature_Fahrenheit, humidity, time.strftime('%Y-%m-%d %H:%M:%S')])
             file.flush()
